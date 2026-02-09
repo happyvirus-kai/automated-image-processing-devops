b7bbeccacbcf0f43a29247915293d3694c3a3f34
Yvette Magana <maganayvette8@gmail.com>
2026-02-09 16:31:25 +0800

    Update project files

--- src/face_mask.py
+++ src/face_mask.py
@@ -4,7 +4,7 @@ face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
 
-def face_mask_detection(image):
+def face_mask(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
