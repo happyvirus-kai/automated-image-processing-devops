e6d623eb7086c00b3fc16a09f258c5d23abc8e60
Yvette Magana <maganayvette8@gmail.com>
2026-02-09 16:49:33 +0800

    Update project files

--- src/color_pop.py
+++ src/color_pop.py
@@ -1,15 +1,13 @@
-import cv2
-import numpy as np
-
 def color_pop(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray_bgr = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+    gray = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
 
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    lower_red = np.array([0, 70, 50])
-    upper_red = np.array([10, 255, 255])
+    lower = np.array([35, 50, 50])   # green range
+    upper = np.array([85, 255, 255])
 
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-    result = np.where(mask[:, :, None] == 255, image, gray_bgr)
+    mask = cv2.inRange(hsv, lower, upper)
+    result = image.copy()
+    result[mask == 0] = gray[mask == 0]
 
     return result
--- src/rain_snow.py
+++ src/rain_snow.py
@@ -1,21 +1,36 @@
-import cv2
+mport cv2
 import numpy as np
 import random
 
 def rain_snow(image, mode="rain"):
     output = image.copy()
     h, w, _ = output.shape
+    rain_layer = np.zeros_like(output, dtype=np.uint8)
 
     if mode == "rain":
+        for _ in range(1200):
+            x = random.randint(0, w - 1)
+            y = random.randint(0, h - 1)
+            length = random.randint(10, 20)
+            cv2.line(
+                rain_layer,
+                (x, y),
+                (x + random.randint(-2, 2), y + length),
+                (200, 200, 200),
+                1
+            )
+
+        rain_layer = cv2.GaussianBlur(rain_layer, (5, 5), 0)
+        output = cv2.addWeighted(output, 0.85, rain_layer, 0.3, 0)
+
+    elif mode == "snow":
         for _ in range(800):
             x = random.randint(0, w - 1)
             y = random.randint(0, h - 1)
-            cv2.line(output, (x, y), (x + 2, y + 10), (200, 200, 200), 1)
+            radius = random.randint(1, 2)
+            cv2.circle(rain_layer, (x, y), radius, (255, 255, 255), -1)
 
-    elif mode == "snow":
-        for _ in range(500):
-            x = random.randint(0, w - 1)
-            y = random.randint(0, h - 1)
-            cv2.circle(output, (x, y), 1, (255, 255, 255), -1)
+        rain_layer = cv2.GaussianBlur(rain_layer, (3, 3), 0)
+        output = cv2.addWeighted(output, 0.9, rain_layer, 0.4, 0)
 
     return output
