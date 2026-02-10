679ac0829bf5337539f78f864797ca436e5a8b99
Kyla Alicaway <kailaalicaway@gmail.com>
2026-02-10 21:28:49 +0800

    Fix tests to check input_images and ensure folder exists

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,8 +1,8 @@
 import os
 
 def test_output_folder_exists():
-    assert os.path.exists("output_images")
+    assert os.path.exists("output")
 
 def test_images_processed():
-    files = os.listdir("put_images")
+    files = os.listdir("input")
     assert len(files) > 0
